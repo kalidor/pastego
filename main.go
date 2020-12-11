af124cf8869b2b6ccaecdbaa618a4dfb6bcffe27
kalidor <kalidor@unixed.fr>
2020-12-11 17:14:48 +0100

    Fix time output

--- main.go
+++ main.go
@@ -93,10 +93,16 @@ func addPaste(body, pasteid string, eol int) {
 	fmt.Printf("addPaste called: %s - %s\n", pasteid, body)
 	tmpfn := filepath.Join(TmpDir, pasteid)
 	t := time.Now()
+	t_body := fmt.Sprintf("%d/%02d/%02d %02d:%02d:%02d",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
+
 	tf := t.Add(time.Duration(eol) * time.Minute)
+	tf_body := fmt.Sprintf("%d/%02d/%02d %02d:%02d:%02d",
+		tf.Year(), tf.Month(), tf.Day(),
+		tf.Hour(), tf.Minute(), tf.Second())
 	body = fmt.Sprintf("%s|%s\n",
-		t.Format("2006-01-02 15:03:00"),
-		tf.Format("2006-01-02 15:03:00")) + body
+		t_body, tf_body) + body
 	err := ioutil.WriteFile(tmpfn, []byte(body), 0600)
 	if err != nil {
 		fmt.Println("Cannot write file.", err)
